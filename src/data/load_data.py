c7c752d97215e3bb620669f6e6a1201e9198a575
HenningGC <henning@gruhl.me>
2024-08-02 01:06:08 +0200

    mods

--- src/data/load_data.py
+++ src/data/load_data.py
@@ -1,9 +1,11 @@
-from utils.AWSClientService import AWSClient
+from utils.AWSClientService import AWSService
 
 
 def main():
-    print(AWSClient)
-    print('Test')
+    
+    S3Client = AWSService('s3').client
+    print(S3Client.list_buckets())
+    
 
 if __name__ == "__main__":
     main()
--- src/utils/AWSClientService.py
+++ src/utils/AWSClientService.py
@@ -1,24 +1,32 @@
 import boto3
 import os
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
 
-class AWSClient:
+class AWSService:
+    """
+    A class to create AWS service clients using boto3.
+    """
 
     def __init__(self, service):
-
         self.service = service
+        self.client = None
+        print('shit')
+        dotenv_path = find_dotenv()
+        if dotenv_path:
+            load_dotenv(dotenv_path)
+        else:
+            print("Warning: .env file not found. Ensure environment variables are set.")
 
-    def create_client(self):
+        aws_access_key = os.getenv('AWS_ACCESS_KEY')
+        aws_secret_key = os.getenv('AWS_SECRET_KEY')
+        aws_region = os.getenv('AWS_REGION')
 
-        load_dotenv()
-        self._aws_client = boto3.client(
-            self.service, 
-            aws_access_key_id=os.getenv('AWS_ACCESS_KEY'),
-            aws_secret_access_key=os.getenv('AWS_SECRET_KEY'),
-            region_name=os.getenv('AWS_REGION')
+        if not aws_access_key or not aws_secret_key or not aws_region:
+            raise ValueError("AWS credentials or region are not set in environment variables.")
+
+        self.client = boto3.client(
+            self.service,
+            aws_access_key_id=aws_access_key,
+            aws_secret_access_key=aws_secret_key,
+            region_name=aws_region
         )
-        
-    @property
-    def aws_client(self):
-        return self._aws_client
-
