a7b2272cb66c56ca557ad9ca6cd243c94728b844
HenningGC <henning@gruhl.me>
2024-08-02 22:58:37 +0200

    more changesffs

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import yaml
-from src.AWSHandler import AWSClientConfig, AWSClient
-from src.dataset_manager import FileHandler
-from dotenv import load_dotenv, find_dotenv
 import os
+import json
 
-
+with open('data/natural/naturalAudio.json','r') as file:
+    print(json.load(file)[0])
