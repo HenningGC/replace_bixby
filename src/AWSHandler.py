45bfe8f461eab70c319d9ed11b43a1c9626e380a
HenningGC <henning@gruhl.me>
2024-08-04 17:33:46 +0200

    modifications in training pipeline

--- src/AWSHandler.py
+++ src/AWSHandler.py
@@ -34,30 +34,3 @@ class AWSClient:
 
     def get_client(self) -> BaseClient:
         return self.client
-
-
-class S3Downloader:
-
-    def __init__(self, bucket_name: str, prefix: str, output_dir: str, config: AWSClientConfig):
-        load_dotenv(find_dotenv())
-        self.bucket_name = bucket_name
-        self.prefix = prefix
-        self.output_dir = output_dir
-        self.s3_client = AWSClient(config=config).get_client()
-
-
-    def list_files(self):
-        response = self.s3_client.list_objects_v2(Bucket=bucket_name, Prefix=prefix)
-        return [obj['Key'] for obj in response.get('Contents', [])][1:]
-
-    def download_file(self, s3_file_name: str, output_file_name: str):
-        self.s3_client.download_file(bucket_name, s3_file_name, output_file_name)
-
-    def process_files(self,fileProcessor):
-        files = self.list_files()
-
-        for s3_file_name in files:
-            file_name = s3_file_name.split('/')[-1]
-            output_file_name = self.output_dir
-            self.download_file(s3_file_name, output_file_name)
-            fileProcessor(output_file_name)
